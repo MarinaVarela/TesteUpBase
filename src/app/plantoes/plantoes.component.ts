46a79ee8f075f650c10b4b180c60cb39947c55a4
Marina Varela <marinavareladev@gmail.com>
2022-11-02 01:05:58 -0300

    Tela em AngularJS

---
+++ src/app/plantoes/plantoes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-plantoes',
+  templateUrl: './plantoes.component.html',
+  styleUrls: ['./plantoes.component.css']
+})
+export class PlantoesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
